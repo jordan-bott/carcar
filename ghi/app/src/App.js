2c1738383d1c111862fb1c67b2568eceea30309a
Jordan Bott <jordan.bott77@gmail.com>
2023-03-06 16:00:02 -0800

    added to app.js and index.js to avoid merge conflicts :D

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -2,13 +2,66 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import MainPage from './MainPage';
 import Nav from './Nav';
 
-function App() {
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+function App(props) {
   return (
     <BrowserRouter>
       <Nav />
       <div className="container">
         <Routes>
           <Route path="/" element={<MainPage />} />
+          <Route path="inventory">
+            <Route path="manufacturers">
+              <Route path="" />
+              <Route path="new" />
+            </Route>
+            <Route path="models">
+              <Route path="" />
+              <Route path="new" />
+            </Route>
+            <Route path="automobile">
+              <Route path="" />
+              <Route path="new" />
+            </Route>
+          </Route>
+          <Route path="sales">
+            <Route path="" />
+            <Route path="new" />
+            <Route path="salesperson">
+              <Route path="new" />
+              <Route path="history" />
+            </Route>
+          </Route>
+          <Route path="service">
+            <Route path="customers/new" />
+            <Route path="technicians/new" />
+            <Route path="appointments">
+              <Route path="" />
+              <Route path="new" />
+            </Route>
+            <Route path="history" />
+          </Route>
         </Routes>
       </div>
     </BrowserRouter>
--- ghi/app/src/index.js
+++ ghi/app/src/index.js
@@ -8,3 +8,63 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+// sales, customer, sales person
+
+// technician, appointment,
+
+//automobile, manufactuerer, vehicle model
+
+async function loadData() {
+
+    const automobileResponse = await fetch('http://localhost:8100/api/automobiles/');
+    const manufacturerResponse = await fetch('http://localhost:8100/api/manufacturers/');
+    const vehicleModelResponse = await fetch('http://localhost:8100/api/models/')
+
+    // const salesResponse = await fetch('')
+    // const customerResponse = await fetch('')
+    // const salesPersonResponse = await fetch('')
+
+    // const technicianResponse = await fetch('')
+    // const appointmentResponse = await fetch('')
+
+    try { if (
+        automobileResponse.ok &&
+        manufacturerResponse.ok &&
+        vehicleModelResponse.ok &&
+        // salesResponse.ok &&
+        // customerResponse.ok &&
+        // salesPersonResponse.ok &&
+        // technicianResponse.ok &&
+        // appointmentResponse.ok
+    ){
+
+        const automobile = await automobileResponse.json();
+        const manufacturer = await manufacturerResponse.json();
+        const vehicleModel = await vehicleModelResponse.json();
+        // const sales = await salesResponse.json();
+        // const customer = await customerResponse.json();
+        // const salesPerson = await salesPersonResponse.json();
+        // const technician = await technicianResponse.json();
+        // const appointment = await appointmentResponse.json();
+
+
+        root.render(
+            <React.StrictMode>
+                <App
+                    automobiles={automobile.automobiles}
+                    manufacturers={manufacturer.manufacturers}
+                    vehicleModels={vehicleModel.vehicleModels}
+                    // sales={sales.sales}
+                    // customers={customer.customers}
+                    // salesPersons={salesPerson.salesPersons}
+                    // technicians={technician.technicians}
+                    // appointments={appointment.appointments}
+                />
+            </React.StrictMode>
+            );
+    }} catch(e) {
+        console.error(e);
+    }
+
+}
